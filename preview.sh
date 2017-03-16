8437f25e949772320a34aacdcc0311ad4c31da07
illy <illy@fout.jp>
2017-03-16 14:55:35 +0900

    Show encoded preview

--- preview.sh
+++ preview.sh
@@ -12,6 +12,10 @@ rm -f "$preview_first" "$preveiw_mid" "$preview_last"
 let "last_frame = $last_frame"
 let "mid_frame = $last_frame / 2"
 let "first_frame=0"
-ffmpeg -ss $first_frame -i $file_path -s 1920x1080 -b:v $bitrate -vframes 1 $preview_first
-ffmpeg -ss $mid_frame -i $file_path -s 1920x1080 -b:v $bitrate -vframes 1 $preview_mid
-ffmpeg -ss $last_frame -i $file_path -s 1920x1080 -b:v $bitrate -vframes 1 $preview_last
+ffmpeg -ss $first_frame -i $file_path -t 1 -s 1920x1080 -b:v $bitrate ./temp/preview_first_temp.mp4
+ffmpeg -ss $mid_frame -i $file_path -t 1 -s 1920x1080 -b:v $bitrate ./temp/preview_mid_temp.mp4
+ffmpeg -ss $last_frame -i $file_path -t 1 -s 1920x1080 -b:v $bitrate ./temp/preview_last_temp.mp4
+ffmpeg -ss 0 -i ./temp/preview_first_temp.mp4 -vframes 1 $preview_first
+ffmpeg -ss 0 -i ./temp/preview_mid_temp.mp4 -vframes 1 $preview_mid
+ffmpeg -ss 0 -i ./temp/preview_last_temp.mp4 -vframes 1 $preview_last
+mv $preview_first $preview_mid $preview_last temp
